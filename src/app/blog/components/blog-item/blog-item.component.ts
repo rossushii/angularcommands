64963bb0553ab5ec598749910aee9b89d749a215
rossushii <raspadernilla@gmai.com>
2024-03-12 23:36:06 +0800

    angular command

---
+++ src/app/blog/components/blog-item/blog-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-blog-item',
+  standalone: true,
+  imports: [],
+  templateUrl: './blog-item.component.html',
+  styleUrl: './blog-item.component.css'
+})
+export class BlogItemComponent {
+
+}
